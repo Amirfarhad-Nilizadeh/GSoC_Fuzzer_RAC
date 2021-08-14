0d34ffda78ccdff1a2a36aff26797010c1103633
Amirfarhad-Nilizadeh <amirfarhad.nilizadeh@gmail.com>
2021-08-14 17:56:03 -0400

    update

---
+++ HyDiff/AddLoop/symexe/src/SymbcDriver.java
@@ -0,0 +1,37 @@
+     import java.io.File;
+     import java.io.FileInputStream;
+     import java.io.IOException;
+     import java.nio.ByteBuffer;
+     import gov.nasa.jpf.symbc.Debug;
+     public class SymbcDriver {	
+     public static void main(String args[]) {
+		int num1 = 0;
+		int num2 = 0;
+	 	if (args.length == 1) {
+           		
+		try (FileInputStream fis = new FileInputStream(args[0])) {
+			byte[] bytes = new byte[Integer.BYTES];
+
+			if (fis.read(bytes) != -1) 
+				num1 = Debug.addSymbolicInt(ByteBuffer.wrap(bytes).getInt(), "sym_num1");
+
+			if (fis.read(bytes) != -1) 
+				num2 = Debug.addSymbolicInt(ByteBuffer.wrap(bytes).getInt(), "sym_num2");
+		} catch (IOException e) {
+           		System.err.println("Error reading input");
+            		e.printStackTrace();
+            		return;
+		}
+
+				 
+	       } else {
+            		num1 = Debug.makeSymbolicInteger("sym_num1");
+            		num2 = Debug.makeSymbolicInteger("sym_num2");
+              }
+        
+        	System.out.println("num1=" + num1);
+        	System.out.println("num2=" + num2);
+
+		AddLoop add = new AddLoop();
+		add.AddLoop(num1, num2);
+}
